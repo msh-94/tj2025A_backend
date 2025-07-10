45452f44bcaf46b50490c2b4aeb1594f22edb0c1
l-yeong <yahn10@naver.com>
2025-07-10 13:14:32 +0900

    자바9일차

--- src/day09/CircleCalculator.java
+++ src/day09/CircleCalculator.java
@@ -1,7 +1,7 @@
 package day09; // 패키지명
 
 public class CircleCalculator { // 클래스 시작
-    final double PI = 3.14159; // 상수
+    static final double PI = 3.14159; // 상수
     double printCircleArea(int 반지름){
         double 원의넓이 = 반지름*반지름*PI;
         return 원의넓이;
