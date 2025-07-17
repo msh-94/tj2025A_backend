481049f4dcbf8d8f62457eb0ba0c869df6eb82c6
msh-94 <alstjdgh2003@naver.com>
2025-07-17 15:20:48 +0900

    자바 14일차 인터페이스 추상메소드

---
+++ src/day14/TestInterface.java
@@ -0,0 +1,25 @@
+package day14; // 패키지명
+
+public interface TestInterface {
+
+    // [6] 상수 , public static final , 생략가능
+    // 주의할점 : 상수는 무조건 초기값이 있어야한다
+    public static final int value2 = 10;
+    int value3 = 20; // 키워드 생략한 상수
+
+    // [7] 추상메소드 : 선언부만 선언하고 실행문{}없는 메소드
+    // public abstract 반환타입 메소드();
+    // 실행문{}은 각  클래스가 구현(오버라이딩) 한다
+    public abstract void method2();
+    boolean method3(int a); // 키워드 생략한 추상메소드
+
+
+    // [3] 멤버변수 , **불가능**
+    // int value1;
+
+    // [4] 생성자   , **불가능**
+    // TestInterface(){}
+
+    // [5] 메소드  , ** 불가능**
+    // void method1(){}
+}// interface end
