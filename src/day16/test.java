e69c26b4d32344b9c0a1fd928f16244f4a32d4c4
msh-94 <alstjdgh2003@naver.com>
2025-07-21 18:17:33 +0900

    자바16일차 실습15 문제4번 input으로 한거 추가

--- src/day16/test.java
+++ src/day16/test.java
@@ -14,15 +14,15 @@ public class test {
             String name = scan.next();
             String outname = name + "님이 방문했습니다.\n";
             File file = new File(path1);
-            if (file.exists()) {
-                FileInputStream fin = new FileInputStream(path1);
-                byte[] inByte = new byte[(int)file.length()];
+            if (file.exists()) { // 파일이 존재하면
+                FileInputStream fin = new FileInputStream(path1); // 읽을 경로 가져오고
+                byte[] inByte = new byte[(int)file.length()]; // 파일용량만큼 배열길이 늘리고
                 fin.read(inByte); // 읽고 나서 추가해야됨 안그러면 이전값을 날려버림
-                String str = new String(inByte);
-                str += outname;
-                byte[] outByte = str.getBytes();
-                FileOutputStream fos = new FileOutputStream(path1);
-                fos.write(outByte);
+                String str = new String(inByte); // 문자열로 변환
+                str += outname; // 기존에 있던것에 새로입력값 추가
+                byte[] outByte = str.getBytes(); // 바이트배열 변환
+                FileOutputStream fos = new FileOutputStream(path1); // 쓰기 할 경로
+                fos.write(outByte); // 쓰기
             }else {
                 FileOutputStream fos = new FileOutputStream(path1);
                 fos.write(outname.getBytes());
