ccfe9016435a862fd706b06dd38947d456489504
msh-94 <alstjdgh2003@naver.com>
2025-07-22 12:27:01 +0900

    자바17일차 예제9 csv 연동,호출,저장함수

--- src/종합/예제9/AppStart.java
+++ src/종합/예제9/AppStart.java
@@ -1,7 +1,23 @@
 package 종합.예제9; // 패키지명
 
+import 종합.예제9.controller.BoardController;
+import 종합.예제9.model.dto.BoardDto;
+
+import java.util.ArrayList;
+
 public class AppStart {// class start
     public static void main(String[] args) { // main start
+
+        // view 작업 전에 controller/dao 테스트
+        // (1) 테스트할 컨트롤러 싱글톤 가져오기
+        BoardController bc = BoardController.getInstance();
+        // (2) 주석 처리 하면서 단위 기능 테스트
+            // 1) 등록 테스트
+        boolean result1 = bc.boardWrite("안녕하세요","유재석");
+        System.out.println( result1 );
+            // 2) 전체조회 테스트
+        ArrayList<BoardDto> result2 = bc.boardPrint();
+        System.out.println( result2 );
 
     }// main end
 }// class end
--- src/종합/예제9/controller/BoardController.java
+++ src/종합/예제9/controller/BoardController.java
@@ -1,6 +1,9 @@
 package 종합.예제9.controller; // 패키지명
 
 import 종합.예제9.model.dao.BoardDao;
+import 종합.예제9.model.dto.BoardDto;
+
+import java.util.ArrayList;
 
 public class BoardController {//class start
     // 싱글톤
@@ -8,7 +11,25 @@ public class BoardController {//class start
     private static final BoardController instance = new BoardController();
     public static BoardController getInstance(){
         return instance;
-    }
-    // dao 가져오기
+    }// func end
+
+    // dao 가져오기 Controller 는 dao만 호출 할수 있다.
     private BoardDao boardDao = BoardDao.getInstance();
+
+    // 등록
+    public boolean boardWrite(String content , String writer){
+        BoardDto boardDto = new BoardDto(content,writer); // 1. 매개변수를 dto객체 생성
+        // 2. 유효성검사(패스) dao에게 저장할 객체를 전달하여 결과 받기
+        boolean result = boardDao.boardWrite(boardDto);
+        return result;
+    }// func end
+
+    // 전체조회
+    public ArrayList<BoardDto> boardPrint(){
+        // 1. dao에게 매개변수없이 모든 dto 정보를 요청하여 받는다
+        ArrayList<BoardDto> result = boardDao.boardPrint();
+        // 2. 결과를 view 에게 리턴
+        return result;
+    }// func end
+
 }// class end
