93a0e70b46ab5928a39a02e603ebda1d4ae29a11
msh-94 <alstjdgh2003@naver.com>
2025-07-22 18:20:40 +0900

    자바17일차 심화과제 db연동하기

--- src/종합/심화과제5/controller/BankController.java
+++ src/종합/심화과제5/controller/BankController.java
@@ -1,5 +1,15 @@
 package 종합.심화과제5.controller; // 패키지명
 
-public class BankController { // class start
+import 종합.심화과제5.model.dao.AccountDao;
+import 종합.심화과제5.model.dao.AccountLogDao;
 
+public class BankController { // class start
+    // 싱글톤
+    private BankController(){}
+    private static final BankController instance = new BankController();
+    public static BankController getInstance(){ return instance; }
+
+    // dao 가져오기
+    private AccountLogDao accountLogDao = AccountLogDao.getInstance();
+    private AccountDao accountDao = AccountDao.getInstance();
 }// class end
