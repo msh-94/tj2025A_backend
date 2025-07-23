91b5aad698ccfad4d26176e0a4a168698a4b8ad7
msh-94 <alstjdgh2003@naver.com>
2025-07-23 16:46:50 +0900

    자바18일차 과제8 db

--- src/종합/과제8/DB연동.sql
+++ src/종합/과제8/DB연동.sql
@@ -1,7 +1,7 @@
 drop database if exists mydb0722;
 create database mydb0722;
 use mydb0722;
-
+set SQL_SAFE_UPDATES = 0;
 
 create table Waiting(
 	wno int auto_increment,
@@ -9,5 +9,12 @@ create table Waiting(
     count int not null ,
     constraint primary key(wno)
 );
+
+insert into waiting(phone,count) values('010-1111-1111',1);
+insert into waiting(phone,count) values('010-2222-2222',2);
+insert into waiting(phone,count) values('010-3333-3333',3);
+insert into waiting(phone,count) values('010-4444-4444',4);
+insert into waiting(phone,count) values('010-5555-5555',5);
+
 select * from waiting;
   
