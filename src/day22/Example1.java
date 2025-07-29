c3fb89ba4b3ec2411bc4b5e2a4425953320f5882
msh-94 <alstjdgh2003@naver.com>
2025-07-29 11:08:00 +0900

    자바22일차 String 클래스

--- src/day22/Example1.java
+++ src/day22/Example1.java
@@ -54,19 +54,23 @@ public class Example1 {// class start
         String str9 = "자바".concat("프로그래밍");  // 자바프로그래밍
         System.out.println(str9);
         System.out.println("자바"+"프로그래밍");        // 자바프로그래밍
+
         // 2) StringBuilder 클래스 : 문자열을 단계적으로 연결 하는 메소드 제공하는 클래스
         StringBuilder builder = new StringBuilder();
         builder.append("자바");
         builder.append("프로그래밍");
         System.out.println(builder);                            // 자바프로그래밍 // 메모리 효율 좋다.
+
         // 3) String.format( "형식문자" , "문자열A" );
         String str10 = String.format("%s%s","자바","프로그래밍");
         System.out.println(str10);                  // 자바프로그래밍
+
         // 4) +연결연산자
         String str11 = "";
         str11 += "자바";
         str11 += "프로그래밍";
         System.out.println(str11);                 // 자바프로그래밍
+
         // 활용] JDBC SQL의 매개변수 연결
         String name = "유재석";
         String sql1 = "insert into table(name) values("+name+")"; // 비권장
@@ -78,6 +82,47 @@ public class Example1 {// class start
         StringBuilder sql4 = new StringBuilder();
         sql4.append("insert into table(name) ");
         sql4.append("values("+name+")");
+
+        // 5) .charAt( 인덱스 ) : 지정한 인덱스 번호의 문자 1개 반환 메소드
+        char gender = "012345-1230123".charAt(7);
+        System.out.println(gender); // 1
+        // 활용] nextChar()[x] --> 문자1개입력 : scan.next().charAt(0)
+
+        // 6) .length() : 문자열내 문자수 반환 메소드
+        System.out.println("012345-1230123".length()); // 인덱스 : 0~13 , 길이 14
+        // 활용] for(int i = 0; i <= 문자열.length(); i++){ }
+
+        // 7) .replace( 기존문자열 , 새로운문자열 ) , replaceAll( 기존문자열 , 새로운문자열 )
+        // : 지정한 문자열내 기존문자열을 새로운문자열 생성(NEW)후 반환 메소드
+        String str12 = "자바프로그래밍".replace("자바","JAVA");
+        System.out.println(str12); // JAVA프로그래밍
+        // 활용] HTML 줄바꿈 <br> ---------> JAVA 줄바꿈 \n
+        String htmlData = "유재석<br/>안녕하세요";
+        String newData = htmlData.replaceAll("<br/>","\n");
+        System.out.println( newData );
+
+        // 8) .subString( 시작인덱스 , [끝인덱스전] ); : 시작인덱스부터 끝인덱스 전 까지 문자열 추출
+        String str13 = "012345-1230123".substring(0,6); // 0번인덱스 부터 6번 인덱스까지
+        System.out.println(str13); // 012345
+        String str14 = "012345-1230123".substring(7); // 7번인덱스 부터 (마지막인덱스)까지 추출
+        System.out.println(str14); // 1230123
+        // 활용] 차량번호 조회 : 214가7531 , 차량번호 뒷자리 : 차량번호.subString(차량번호.length()-1-3);
+
+        // 9) .split( "구분문자" ); : 지정한 문자열내 구분문자 기준으로 잘라서 배열로 반환 메소드
+        String[] str15 = "012345-1230123".split("-"); // '-' 기준으로 문자열 쪼개기
+        System.out.println(str15[0]); // 012345
+        System.out.println(str15[1]); // 1230123
+        // 활용1] CSV형식 다루기 , csv란 ,(쉼표)로 속성을 구분하고 \n 줄바꿈 구분 한다
+        // 활용2] 날짜(연-월-일)
+        // 활용3] 주소( 시 군 구 동 )
+
+        // 10) .indexOf("찾을문자열"); : 지정한 문자열내 찾을문자열이 존재하면 찾은 인덱스 , 없으면 -1 반환 메소드
+        int findIndex = "자바 프로그래밍 언어".indexOf("프로");
+        System.out.println(findIndex); // 3
+        // 11) .contains("찾을문자열"); : 지정한 문자열내 찾을문자열이 존재하면 true 없으면 false 반환 메소드
+        boolean findBool = "자바 프로그래밍 언어".contains("프로");
+        System.out.println(findBool); // true
+        // 활용] 검색기능 구현
 
 
 
